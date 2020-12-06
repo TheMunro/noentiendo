eaabfd2689c71ad9d369cd6291caeb7923c2d5eb
James Munro <james.i.munro@gmail.com>
2020-12-06 15:28:05 +0000

    added bus stub functionality

--- nes-emu/inc/bus.hpp
+++ nes-emu/inc/bus.hpp
@@ -1,5 +1,26 @@
+#pragma once
+
+//STDLIB
+#include <array>
+#include <memory>
+#include <cstdint>
+
+namespace nes
+{
+constexpr std::int32_t address_ram_size = 0xFFFF;
+
 class bus
 {
 public:
+	bus()
+		: ram{std::make_unique<std::array<uint8_t, address_ram_size>>()}
+	{
+	}
+	
+	uint8_t read(uint16_t address, bool read_only = false) const;
+	void write(uint16_t address, uint8_t data) const;
+
 private:
+	std::unique_ptr<std::array<uint8_t, address_ram_size>> ram;
 };
+}	 // namespace nes
--- nes-emu/inc/cpu.hpp
+++ nes-emu/inc/cpu.hpp
@@ -1,5 +1,20 @@
+#pragma once
 
+//STDLIB
+#include <memory>
+
+//INTERNAL
+#include "bus.hpp"
+
+namespace nes
+{
 class cpu
 {
-	
+	cpu()
+		: bus{std::make_unique<nes::bus>()}
+	{
+	}
+
+	std::unique_ptr<bus> bus;
 };
+}	 // namespace nes
