ffd2784f87c4f1e1ece717c9602c98d0c71e7644
James Munro <james.i.munro@gmail.com>
2020-12-09 02:35:38 +0000

    fixed a whoops

--- nes-emu/inc/bus.hpp
+++ nes-emu/inc/bus.hpp
@@ -13,16 +13,16 @@ class bus
 {
 public:
 	bus()
-		: ram{std::make_unique<std::array<uint8_t, address_ram_size>>()}
+		: ram{std::make_unique<std::array<std::uint8_t, address_ram_size>>()}
 	{
 	}
 	
-	[[nodiscard]] uint8_t read(uint16_t address, bool read_only = false) const;
-	void write(uint16_t address, uint8_t data) const;
+	[[nodiscard]] std::uint8_t read(std::uint16_t address, bool read_only = false) const;
+	void write(std::uint16_t address, std::uint8_t data) const;
 
 private:
 	//hmm... this should possibly be another peripheral in the nes class
 	//and registered with the bus, since the peripheral use the bus, but aren't owned by it?
-	std::unique_ptr<std::array<uint8_t, address_ram_size>> ram;
+	std::unique_ptr<std::array<std::uint8_t, address_ram_size>> ram;
 };
 }	 // namespace nes
--- nes-emu/inc/cpu.hpp
+++ nes-emu/inc/cpu.hpp
@@ -34,11 +34,10 @@ class instruction
 public:
 	opcode opcode;
 	bool complete = false;
-	//uint8_t size;
-	//uint8_t duration;
+	uint8_t cycles = 0;
 
-	uint8_t (cpu::*execute)() = nullptr;
-	uint8_t (cpu::*addressing_mode)() = nullptr;
+	bool (cpu::*execute)() = nullptr;
+	bool (cpu::*addressing_mode)() = nullptr;
 };
 
 
@@ -58,12 +57,12 @@ public:
 	void non_maskable_interrupt();
 
 	//data
-	[[nodiscard]] uint8_t fetch();
-	[[nodiscard]] uint8_t read(uint16_t address, bool read_only = false) const;
-	void write(uint16_t address, uint8_t data) const;
-
+	[[nodiscard]] std::uint8_t fetch();
+	[[nodiscard]] std::uint8_t read(std::uint16_t address, bool read_only = false) const;
+	void write(std::uint16_t address, std::uint8_t data) const;
 
 	//addressing
+	//http://www.emulator101.com/6502-addressing-modes.html
 	bool address_mode_implicit();
 	bool address_mode_accumulator();
 	bool address_mode_immediate();
@@ -80,9 +79,64 @@ public:
 	bool address_mode_indirect();
 	bool address_mode_indexed_indirect();
 	bool address_mode_indirect_indexed();
-
-
+	
 	//opcodes
+	bool instruction_adc();
+	bool instruction_and();
+	bool instruction_asl();
+	bool instruction_bcc();
+	bool instruction_bcs();
+	bool instruction_beq();
+	bool instruction_bit();
+	bool instruction_bmi();
+	bool instruction_bne();
+	bool instruction_bpl();
+	bool instruction_brk();
+	bool instruction_bvc();
+	bool instruction_bvs();
+	bool instruction_clc();
+	bool instruction_cld();
+	bool instruction_cli();
+	bool instruction_clv();
+	bool instruction_cmp();
+	bool instruction_cpx();
+	bool instruction_cpy();
+	bool instruction_dec();
+	bool instruction_dex();
+	bool instruction_dey();
+	bool instruction_eor();
+	bool instruction_inc();
+	bool instruction_inx();
+	bool instruction_iny();
+	bool instruction_jmp();
+	bool instruction_jsr();
+	bool instruction_lda();
+	bool instruction_ldx();
+	bool instruction_ldy();
+	bool instruction_lsr();
+	bool instruction_nop();
+	bool instruction_ora();
+	bool instruction_pha();
+	bool instruction_php();
+	bool instruction_pla();
+	bool instruction_plp();
+	bool instruction_rol();
+	bool instruction_ror();
+	bool instruction_rti();
+	bool instruction_rts();
+	bool instruction_sbc();
+	bool instruction_sec();
+	bool instruction_sed();
+	bool instruction_sei();
+	bool instruction_sta();
+	bool instruction_stx();
+	bool instruction_sty();
+	bool instruction_tax();
+	bool instruction_tay();
+	bool instruction_tsx();
+	bool instruction_txa();
+	bool instruction_txs();
+	bool instruction_tya();
 	
 private:
 	[[nodiscard]] static constexpr std::array<instruction, 256> build_instructions();
@@ -91,19 +145,19 @@ private:
 	const std::array<instruction, 256> instructions;
 
 	//registers
-	uint8_t register_accumulator = 0x00; 
-	uint8_t register_x = 0x00;
-	uint8_t register_y = 0x00;
-	uint8_t register_stack_pointer = 0x00;
-	uint16_t register_program_counter = 0x0000;
+	std::uint8_t register_accumulator = 0x00; 
+	std::uint8_t register_x = 0x00;
+	std::uint8_t register_y = 0x00;
+	std::uint8_t register_stack_pointer = 0x00;
+	std::uint16_t register_program_counter = 0x0000;
 	processor_status_register register_status = processor_status_register::none;
 
 	//internals
-	uint8_t fetched = 0x00;                //working data for current instruction, if required
-	uint16_t address_absolute = 0x0000;    //used for direct addressing
-	uint8_t address_relative = 0x00;       //used for branching operations [-127, 128]
-	uint8_t opcode = 0x00;                 //current instruction
-	uint8_t cycles_remaining = 0;          //remaining cycles for current instruction
-	uint32_t clock_count = 0;              //clock count accumulator
+	std::uint8_t fetched = 0x00;                //working data for current instruction, if required
+	std::uint16_t address_absolute = 0x0000;    //used for direct addressing
+	std::uint16_t address_relative = 0x0000;	   //used for branching operations [-127, 128]
+	std::uint8_t opcode = 0x00;                 //current instruction
+	std::uint8_t cycles_remaining = 0;          //remaining cycles for current instruction
+	std::uint32_t clock_count = 0;              //clock count accumulator
 };
 }	 // namespace nes
