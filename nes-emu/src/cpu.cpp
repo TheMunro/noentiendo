1b7a42d71a739424b4398d46da9439554e1a71dd
James Munro <james.i.munro@gmail.com>
2020-12-09 02:34:13 +0000

    addressing modes and first-pass of cpu clock function

--- nes-emu/src/cpu.cpp
+++ nes-emu/src/cpu.cpp
@@ -1,26 +1,50 @@
 //INTERNAL
 #include "cpu.hpp"
 
-uint8_t nes_emu::cpu::read(const uint16_t address, bool read_only) const
+std::uint8_t nes_emu::cpu::read(const std::uint16_t address, bool read_only) const
 {
 	return bus.read(address, read_only);
 }
 
-void nes_emu::cpu::write(const uint16_t address, const uint8_t data) const
+void nes_emu::cpu::write(const std::uint16_t address, const std::uint8_t data) const
 {
 	return bus.write(address, data);
+}
+
+void nes_emu::cpu::clock()
+{
+	if (cycles_remaining == 0)
+	{
+		opcode = read(register_program_counter);
+		register_program_counter++;
+
+		const auto& instruction = instructions[opcode];
+		cycles_remaining = instruction.cycles;
+
+		const auto additional_cycle_addressing_mode = (this->*instruction.addressing_mode)();
+		const auto additional_cycle_execute = (this->*instruction.execute)();
+
+		cycles_remaining += (additional_cycle_addressing_mode & additional_cycle_execute);
+	}
+
+	clock_count++;
+	cycles_remaining--;
 }
 
 constexpr std::array<nes_emu::instruction, 256> nes_emu::cpu::build_instructions()
 {
 	std::array<instruction, 256> instructions{};
-	instructions[0x00] = instruction{opcode::BRK, false, nullptr, nullptr};
+
+	instructions[0x00] = instruction{opcode::BRK, false, 4, &cpu::instruction_adc, &cpu::address_mode_absolute_x};
 	
 	return instructions;
 }
 
 bool nes_emu::cpu::address_mode_implicit()
 {
+	//In an implied instruction, the data and/or destination is mandatory for the instruction. For example,
+	//the CLC instruction is implied, it is going to clear the processor's Carry flag.
+	
 	//working data determined by instruction
 	
 	//program counter increment controlled by instruction
@@ -29,6 +53,8 @@ bool nes_emu::cpu::address_mode_implicit()
 
 bool nes_emu::cpu::address_mode_accumulator()
 {
+	//These instructions have register A (the accumulator) as the target.
+	
 	//accumulator addressing modes skip fetch step as register value is already populated
 	//set current working data to accumulator
 	fetched = register_accumulator;
@@ -39,6 +65,9 @@ bool nes_emu::cpu::address_mode_accumulator()
 
 bool nes_emu::cpu::address_mode_immediate()
 {
+	//These instructions have their data defined as the next byte after the opcode. ORA #$B2 will perform a
+	//logical (also called bitwise) of the value B2 with the accumulator.
+	//
 	//cache current program data as address
 	address_absolute = register_program_counter;
 	
@@ -49,6 +78,12 @@ bool nes_emu::cpu::address_mode_immediate()
 
 bool nes_emu::cpu::address_mode_zero_page()
 {
+	//Zero-Page is an addressing mode that is only capable of addressing the first 256 bytes of the CPU's memory map.
+	//You can think of it as absolute addressing for the first 256 bytes. The instruction LDA $35 will put the value
+	//stored in memory location $35 into A. The advantage of zero-page are two - the instruction takes one less byte
+	//to specify, and it executes in less CPU cycles. Most programs are written to store the most frequently used
+	//variables in the first 256 memory locations so they can take advantage of zero page addressing.
+	
 	const auto low_byte = read(register_program_counter);
 	address_absolute = low_byte;
 	
@@ -59,6 +94,11 @@ bool nes_emu::cpu::address_mode_zero_page()
 
 bool nes_emu::cpu::address_mode_zero_page_x()
 {
+	//This works just like absolute indexed, but the target address is limited to the first 0xFF bytes.
+	//The target address will wrap around and will always be in the zero page.If the instruction is LDA $C0, X,
+	//and X is $60, then the target address will be $20.$C0 + $60 = $120, but the carry is discarded in the
+	//calculation of the target address.
+	
 	const auto low_byte = read(register_program_counter);
 	address_absolute = (low_byte + register_x) & 0x00FF;
 
@@ -69,6 +109,11 @@ bool nes_emu::cpu::address_mode_zero_page_x()
 
 bool nes_emu::cpu::address_mode_zero_page_y()
 {
+	//This works just like absolute indexed, but the target address is limited to the first 0xFF bytes.
+	//The target address will wrap around and will always be in the zero page.If the instruction is LDA $C0, X,
+	//and X is $60, then the target address will be $20.$C0 + $60 = $120, but the carry is discarded in the
+	//calculation of the target address.
+	
 	const auto low_byte = read(register_program_counter);
 	address_absolute = (low_byte + register_y) & 0x00FF;
 
@@ -79,6 +124,10 @@ bool nes_emu::cpu::address_mode_zero_page_y()
 
 bool nes_emu::cpu::address_mode_relative()
 {
+	//Relative addressing on the 6502 is only used for branch operations. The byte after the opcode
+	//is the branch offset. If the branch is taken, the new address will the the current PC plus the offset.
+	//The offset is a signed byte, so it can jump a maximum of 127 bytes forward, or 128 bytes backward.
+
 	address_relative = read(register_program_counter);
 	register_program_counter++;
 	
@@ -90,10 +139,15 @@ bool nes_emu::cpu::address_mode_relative()
 
 bool nes_emu::cpu::address_mode_absolute()
 {
-	const uint16_t lo_byte = read(register_program_counter);
+	//Absolute addressing specifies the memory location explicitly in the two bytes following the
+	//opcode. So JMP $4032 will set the PC to $4032. The hex for this is 4C 32 40.
+	//The 6502 is a little endian machine, so any 16 bit (2 byte) value is stored with the LSB first.
+	//All instructions that use absolute addressing are 3 bytes.
+	
+	const std::uint16_t lo_byte = read(register_program_counter);
 	register_program_counter++;
 
-	const uint16_t hi_byte = read(register_program_counter);
+	const std::uint16_t hi_byte = read(register_program_counter);
 	register_program_counter++;
 
 	address_absolute = (hi_byte << 0xFF) + lo_byte;
@@ -103,6 +157,10 @@ bool nes_emu::cpu::address_mode_absolute()
 
 bool nes_emu::cpu::address_mode_absolute_x()
 {
+	//This addressing mode makes the target address by adding the contents of the X or Y
+	//register to an absolute address. For example, this 6502 code can be used to fill 10 bytes
+	//with $FF starting at address $1009, counting down to address $1000.
+	
 	address_mode_absolute();
 
 	const auto hi_byte = address_absolute & 0xFF00;
@@ -115,27 +173,92 @@ bool nes_emu::cpu::address_mode_absolute_x()
 
 bool nes_emu::cpu::address_mode_absolute_y()
 {
+	//This addressing mode makes the target address by adding the contents of the X or Y
+	//register to an absolute address. For example, this 6502 code can be used to fill 10 bytes
+	//with $FF starting at address $1009, counting down to address $1000.
+	
 	address_mode_absolute();
 
 	const auto hi_byte = address_absolute & 0xFF00;
 	address_absolute += register_y;
 	if ((hi_byte & (address_absolute & 0xFF00)) == 0xFF00)
-		return true;	//new page, use result to increase cycle count
+		return true; //new page, use result to increase cycle count
 
 	return false;
 }
 
 bool nes_emu::cpu::address_mode_indirect()
 {
+	//The JMP instruction is the only instruction that uses this addressing mode.
+	//It is a 3 byte instruction - the 2nd and 3rd bytes are an absolute address.
+	//The set the PC to the address stored at that address. So maybe this would be clearer.
+
+	const std::uint16_t lo_byte = read(register_program_counter);
+	register_program_counter++;
+
+	const std::uint16_t hi_byte = read(register_program_counter);
+	register_program_counter++;
+
+	address_absolute = (hi_byte << 0xFF) + lo_byte;
+	
 	return false;
 }
 
 bool nes_emu::cpu::address_mode_indexed_indirect()
 {
+	//This mode is only used with the X register. Consider a situation where the instruction is LDA ($20,X),
+	//X contains $04, and memory at $24 contains 0024: 74 20, First, X is added to $20 to get $24.
+	//The target address will be fetched from $24 resulting in a target address of $2074.
+	//Register A will be loaded with the contents of memory at $2074.
+	//
+	//If X + the immediate byte will wrap around to a zero - page address.So you could code that
+	//like targetAddress = (X + opcode[1]) & 0xFF.
+	//
+	//Indexed Indirect instructions are 2 bytes - the second byte is the zero - page address - $20 in
+	//the example.Obviously the fetched address has to be stored in the zero page.
+
+	auto ptr = read(register_program_counter);
+	register_program_counter++;
+
+	ptr += register_x;
+
+	const std::uint16_t hi_byte = read(ptr) << 8;
+	const std::uint16_t lo_byte = read(ptr + 1);
+
+	address_absolute = (hi_byte << 0xFF) + lo_byte;
+	
 	return false;
 }
 
 bool nes_emu::cpu::address_mode_indirect_indexed()
 {
+	//This mode is only used with the Y register. It differs in the order that Y is applied to the indirectly
+	//fetched address. An example instruction that uses indirect index addressing is LDA ($86),Y .
+	//To calculate the target address, the CPU will first fetch the address stored at zero page location $86.
+	//That address will be added to register Y to get the final target address. For LDA ($86),Y,
+	//if the address stored at $86 is $4028 (memory is 0086: 28 40, remember little endian) and register Y
+	//contains $10, then the final target address would be $4038. Register A will be loaded with the
+	//contents of memory at $4038.
+	//
+	//Indirect Indexed instructions are 2 bytes - the second byte is the zero - page address - $86 in the
+	//example.(So the fetched address has to be stored in the zero page.)
+	//
+	//While indexed indirect addressing will only generate a zero - page address, this mode's target address
+	//is not wrapped - it can be anywhere in the 16-bit address space.
+
+	const auto ptr = read(register_program_counter);
+	register_program_counter++;
+
+	const std::uint16_t hi_byte = read(ptr) << 8;
+	const std::uint16_t lo_byte = read(ptr + 1);
+
+	address_absolute = (hi_byte << 0xFF) + lo_byte;
+	
+	address_absolute += register_y;
+
+	//check for wraparound
+	if ((address_absolute & 0xFF00) != hi_byte)
+		return true;
+	
 	return false;
 }
